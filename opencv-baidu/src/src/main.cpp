b4f9ae8b4b8e2e7cd3e68c724bb9c16198b20044
drluo <2557657882@qq.com>
2023-11-23 00:01:14 +0800

    添加中线拟合

--- opencv-baidu/src/src/main.cpp
+++ opencv-baidu/src/src/main.cpp
@@ -52,8 +52,8 @@ Mat nitoushi;
 int main() {
 
 	Mat frame;
-	VideoCapture cap(0);
-	//VideoCapture cap("lx.mp4");
+	//VideoCapture cap(0);
+	VideoCapture cap("lx.mp4");
 	cap.set(cv::CAP_PROP_FRAME_WIDTH, 320);
 	cap.set(cv::CAP_PROP_FRAME_HEIGHT, 240);
 	//
@@ -143,6 +143,88 @@ int main() {
 				rpts_num = far_rpts1s_num - far_Lpt1_rpts1s_id;
 			}
 		}
+
+		{
+			// 车轮对应点(纯跟踪起始点)
+			float cx = mapx[COLSIMAGE / 2][(int)(ROWSIMAGE * 0.78f)];
+			float cy = mapy[COLSIMAGE / 2][(int)(ROWSIMAGE * 0.78f)];
+			
+			// 找最近点(起始点中线归一化)
+			float min_dist = 1e10;
+			int begin_id = -1;
+			for (int i = 0; i < rpts_num; i++) {
+				float dx = rpts[i][0] - cx;
+				float dy = rpts[i][1] - cy;
+				float dist = sqrt(dx * dx + dy * dy);
+				if (dist < min_dist) {
+					min_dist = dist;
+					begin_id = i;
+				}
+			}
+		
+			// 特殊模式下，不找最近点(由于边线会绕一圈回来，导致最近点为边线最后一个点，从而中线无法正常生成)
+			if (/*garage_type == GARAGE_IN_LEFT || garage_type == GARAGE_IN_RIGHT || */cross_type == CROSS_IN) begin_id = 0;
+			
+			// 中线有点，同时最近点不是最后几个点
+			if (begin_id >= 0 && rpts_num - begin_id >= 3) {
+				// 归一化中线
+				rpts[begin_id][0] = cx;
+				rpts[begin_id][1] = cy;
+				rptsn_num = sizeof(rptsn) / sizeof(rptsn[0]);
+				resample_points(rpts + begin_id, rpts_num - begin_id, rptsn, &rptsn_num, sample_dist * pixel_per_meter);
+
+				// 远预锚点位置
+				int aim_idx = clip(round(aim_distance / sample_dist), 0, rptsn_num - 1);
+				// 近预锚点位置
+				int aim_idx_near = clip(round(0.25 / sample_dist), 0, rptsn_num - 1);
+
+				// 计算远锚点偏差值
+				float dx = rptsn[aim_idx][0] - cx;
+				float dy = cy - rptsn[aim_idx][1] + 0.2 * pixel_per_meter;
+				float dn = sqrt(dx * dx + dy * dy);
+				float error = -atan2f(dx, dy) * 180 / PI;
+				assert(!isnan(error));
+
+				// 若考虑近点远点,可近似构造Stanley算法,避免撞路肩
+				// 计算近锚点偏差值
+				float dx_near = rptsn[aim_idx_near][0] - cx;
+				float dy_near = cy - rptsn[aim_idx_near][1] + 0.2 * pixel_per_meter;
+				float dn_near = sqrt(dx_near * dx_near + dy_near * dy_near);
+				float error_near = -atan2f(dx_near, dy_near) * 180 / PI;
+				assert(!isnan(error_near));
+
+				// 远近锚点综合考虑
+				//angle = pid_solve(&servo_pid, error * far_rate + error_near * (1 - far_rate));
+				// 根据偏差进行PD计算
+				//float angle = pid_solve(&servo_pid, error);
+
+				//// 纯跟踪算法(只考虑远点)
+				//float pure_angle = -atanf(pixel_per_meter * 2 * 0.2 * dx / dn / dn) / PI * 180 / SMOTOR_RATE;
+				//angle = pid_solve(&servo_pid, pure_angle);
+				//angle = MINMAX(angle, -14.5, 14.5);
+
+				////非上坡电感控制,PD计算之后的值用于寻迹舵机的控制
+				//if (enable_adc) {
+				//	// 当前上坡模式，不控制舵机，同时清除所有标志
+				//	circle_type = CIRCLE_NONE;
+				//	cross_type = CROSS_NONE;
+				//	//garage_type = GARAGE_NONE;
+				//}
+				//else if (adc_cross && cross_type == CROSS_IN) {
+				//	// 当前是十字模式，同时启用了电感过十字，则不控制舵机
+				//}
+				//else {
+				//	//smotor1_control(servo_duty(SMOTOR1_CENTER + angle));
+				//}
+
+			}
+			else {
+				// 中线点过少(出现问题)，则不控制舵机
+				rptsn_num = 0;
+			}
+		
+		}
+
 		clear_image(&img_line);
 		draw_circle();
 		draw_cross();
@@ -161,8 +243,8 @@ int main() {
 #endif
 		// 绘制道路线            
 		for (int i = 0; i < rpts0s_num; i++) {
-				AT_IMAGE(&img_line, clip(rpts0s[i][0], 0, img_line.width - 1),
-					clip(rpts0s[i][1], 0, img_line.height - 1)) = 255;
+			AT_IMAGE(&img_line, clip(rpts0s[i][0], 0, img_line.width - 1),
+				clip(rpts0s[i][1], 0, img_line.height - 1)) = 255;
 		}
 		for (int i = 0; i < rpts1s_num; i++) {
 			AT_IMAGE(&img_line, clip(rpts1s[i][0], 0, img_line.width - 1),
@@ -182,6 +264,7 @@ int main() {
 		if (Lpt1_found) {
 			draw_x(&img_line, rpts1s[Lpt1_rpts1s_id][0], rpts1s[Lpt1_rpts1s_id][1], 3, 255);
 		}
+
 
 		show_line();
 		imshow("frame", frame);
